7647d2fd87c46e3fdcbb53e34697747d7dbd41af
arthurgmr <arthur.gmr@hotmail.com>
2021-03-31 21:10:17 -0300

    routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const routes = express.Router()
 
-const SessionController = require('../app/controllers/SessionController')
 const UserController = require('../app/controllers/UserController')
 
 const UserValidator = require('../app/validators/user')
-const SessionValidator = require('../app/validators/session')
-const { isLoggedRedirectToUsers } = require('../app/middlewares/session')
 const { onlyUsers }= require('../app/middlewares/session')
 
 
@@ -18,7 +15,6 @@ routes.get('/', onlyUsers, UserValidator.index, UserController.index)
 routes.get('/print-form', onlyUsers, UserValidator.index, UserController.printForm)
 routes.get('/edit', onlyUsers, UserController.edit)
 routes.put('/edit', onlyUsers, UserValidator.put, UserController.put)
-// routes.put('/', UserValidator.update, UserController.update)
 // routes.delete('/', UserController.delete)
 
 
