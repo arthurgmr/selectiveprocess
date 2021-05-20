5e6ee470d4034429d697be5e9bdaa4a218668259
arthurgmr <arthur.gmr@hotmail.com>
2021-05-20 14:08:45 -0300

    config-db

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,12 @@
-// banco de dados precisa realizar verificação, 
-// com o pool ele faz essa comunicação autorizada,
-// não sendo preciso realizar toda vez o login e senha;
-const { Pool } = require("pg")
+
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+const connectionString = process.env.DATABASE_URL;
 
 module.exports = new Pool({
-    user: "postgres",
-    password: "agmr1275",
-    host: "localhost",
-    port: 5432,
-    database: "selectiveprocessdb"
-})
+    connectionString,
+});
+
+
 
