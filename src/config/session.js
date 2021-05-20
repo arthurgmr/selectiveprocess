d1f2ca8cc53ec9e65590f5994b8713890db37921
arthurgmr <arthur.gmr@hotmail.com>
2021-05-20 14:32:26 -0300

    fix-session-config

--- src/config/session.js
+++ src/config/session.js
@@ -1,9 +1,11 @@
 const session = require('express-session')
 const pgSession = require('connect-pg-simple') (session)
 
+const connectionString = process.env.DATABASE_URL;
+
 module.exports = session({
     store: new pgSession({
-        conString: process.even.DATABASE_URL
+        conString: connectionString
     }),
     secret: '_0#@!($msT',
     resave: false,
