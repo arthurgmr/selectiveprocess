0257c03878dcd4f989d4cf0719f8f76978430548
Arthur Machado <arthur.gmr@hotmail.com>
2021-05-27 15:25:56 -0300

    integration-mariadb

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,36 @@
-const { createPool } = require("mariadb");
+const mariadb = require("mariadb");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-const connectionString = process.env.DATABASE_URL;
+const db = mariadb.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    connectionLimit: 5
+})
 
-module.exports = new createPool({
-    host: 'localhost',
-    user: 'arthurgmr',
-    password: '123456',
+//conect and check for errors
+db.getConnection((err, connection) => {
+    if(err) {
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection lost');
+        }
+        if(err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connection');
+        }
+        if(err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused');
+        }
+    }
+
+    if(connection) connection.release();
+
+    return;
 });
+
+module.exports = db;
 
 
 
--- src/config/session.js
+++ src/config/session.js
@@ -1,13 +1,13 @@
 const session = require('express-session');
-const db = require('./db');
 const MariaDBStore = require('express-session-mariadb-store')
 
 module.exports = session({
     store: new MariaDBStore({
-        user: 'arthurgmr',
-        password: '123456'
-    }),
-    secret: '_0#@!($msT',
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
+    }),    
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
