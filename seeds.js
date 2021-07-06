17f747180b5084ee41c3a6bf52fa35c7443f59de
Arthur Machado <arthur.gmr@hotmail.com>
2021-07-06 13:53:05 -0300

    seeds

--- seeds.js
+++ seeds.js
@@ -1,14 +1,27 @@
 const faker = require('faker')
+faker.locale = 'pt_BR'
 const { hash } = require('bcryptjs')
 
 const User = require('./src/app/models/User')
-const Product = require('./src/app/models/Product')
-const File = require('./src/app/models/File')
 
-let usersIds = []
-let productsIds = []
-let totalUsers = 3
-let totalProducts = 10
+let totalUsers = 50
+
+const funcs = [
+    "Regular(F01)",
+    "Especial(F02)",
+    "Regular(F01),Especial(F02)"
+]
+
+const periodCourse = [
+    "1° Período/Semestre",
+    "2° Período/Semestre",
+    "3° Período/Semestre",
+    "4° Período/Semestre",
+    "5° Período/Semestre",
+    "6° Período/Semestre",
+    "7° Período/Semestre",
+    "8° Período/Semestre",
+]
 
 async function createUsers() {
     const users = []
@@ -17,11 +30,24 @@ async function createUsers() {
     while (users.length < totalUsers) {
         users.push({
             name: faker.name.firstName(),
-            email: faker.internet.email(),
-            password,
-            cpf_cnpj: faker.random.number(99999999999),
-            cep: faker.random.number(99999999),
+            cpf: faker.random.number(99999999999),
+            birth_date: Date.parse(faker.date.past(18)),
+            deficient: Math.floor(Math.random() * 2),
+            cep: faker.address.zipCode(),
             address: faker.address.streetName(),
+            address_number: faker.random.number(999),
+            address_complement: "",
+            address_district: faker.address.streetName(),
+            course_id: Math.ceil(Math.random() * 4),
+            college_id: Math.ceil(Math.random() * 4),
+            funcs: funcs[Math.floor(Math.random() * 3)],
+            period_course: periodCourse[Math.floor(Math.random() * 8)],
+            specialization_regular: Math.floor(Math.random() * 2),
+            specialization_special: Math.floor(Math.random() * 2),
+            email: faker.internet.email(),
+            phone1: faker.random.number(99999999999),
+            phone2: "",
+            password,
         })
     }
 
@@ -30,43 +56,43 @@ async function createUsers() {
     usersIds = await Promise.all(usersPromise)
 }
 
-async function createProducts() {
-    let products = []
+// async function createProducts() {
+//     let products = []
 
-    while (products.length < totalProducts) {
-        products.push({
-            category_id: Math.ceil(Math.random() * 3),
-            user_id: usersIds[Math.floor(Math.random() * totalUsers)],
-            name: faker.name.title(),
-            description: faker.lorem.paragraph(Math.ceil(Math.random() * 10)),
-            old_price: faker.random.number(9999),
-            price: faker.random.number(9999),
-            quantity: faker.random.number(99),
-            status: Math.round(Math.random())
-        })
-    }
+//     while (products.length < totalProducts) {
+//         products.push({
+//             category_id: Math.ceil(Math.random() * 3),
+//             user_id: usersIds[Math.floor(Math.random() * totalUsers)],
+//             name: faker.name.title(),
+//             description: faker.lorem.paragraph(Math.ceil(Math.random() * 10)),
+//             old_price: faker.random.number(9999),
+//             price: faker.random.number(9999),
+//             quantity: faker.random.number(99),
+//             status: Math.round(Math.random())
+//         })
+//     }
 
-    const productsPromise = products.map(product => Product.create(product))
-    productsIds = await Promise.all(productsPromise)
+//     const productsPromise = products.map(product => Product.create(product))
+//     productsIds = await Promise.all(productsPromise)
 
-    let files = []
+//     let files = []
 
-    while(files.length < 50) {
-        files.push({
-            name: faker.image.image(),
-            path: `public/images/placeholder.png`,
-            product_id: productsIds[Math.floor(Math.random() * totalProducts)]
-        })
-    }
+//     while(files.length < 50) {
+//         files.push({
+//             name: faker.image.image(),
+//             path: `public/images/placeholder.png`,
+//             product_id: productsIds[Math.floor(Math.random() * totalProducts)]
+//         })
+//     }
     
-    const filesPromise = files.map(file => File.create(file))
+//     const filesPromise = files.map(file => File.create(file))
 
-    await Promise.all(filesPromise)
-}
+//     await Promise.all(filesPromise)
+// }
 
 async function init() {
     await createUsers()
-    await createProducts()
+    // await createProducts()
 }
 
 init()
